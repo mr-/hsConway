65a8534b4f8d5317ccdaae3206b9be92f1928ea6
Martin Ruderer <martin.ruderer@gmail.com>
2013-02-18 20:47:14 +0100

    Sane Types + Start Button works (once)

--- src/gtkConwayStatefulButton.hs
+++ src/gtkConwayStatefulButton.hs
@@ -3,14 +3,18 @@
 import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk.Gdk.Events
-import Data.Array.Unboxed
+
+import Data.Array
 import Data.List
 import Control.Monad.State
 import Data.IORef
 import Control.Concurrent as CC
 
-type Grid = UArray (Int,Int) Bool
-type GridDelta = UArray (Int,Int) Int
+data Cell = Dead | Alive deriving (Eq, Show)
+type Grid = Array (Int,Int) Cell
+
+data Delta = Kill | Spawn | Keep deriving (Eq, Show)
+type GridDelta = Array (Int,Int) Delta
 
 wWidth = 720 
 wHeight = 720
@@ -42,11 +46,12 @@ main= do
 
     curGridRef <- newIORef glider
 
-    timeoutAdd ( do curGrid <- readIORef curGridRef
-                    let newGrid = nextGen curGrid 
-                    renderWithDrawable drawin (mainloop (delta curGrid newGrid))
-                    writeIORef curGridRef newGrid
-                    return True) 300
+
+    onClicked button1 (timeoutAdd (      do curGrid <- readIORef curGridRef
+                                            let newGrid = nextGen curGrid 
+                                            renderWithDrawable drawin (mainloop (delta curGrid newGrid))
+                                            writeIORef curGridRef newGrid
+                                            return True) 300 >> return ())
 
     onButtonPress canvas 
                 (\x -> do   let (a, b) = coordToCell (eventX x) (eventY x)
@@ -68,9 +73,9 @@ main= do
 mainloop :: GridDelta -> Render ()
 mainloop gridDelta = do
     let b = bounds gridDelta
-    forM_ (range b) (\(x,y) -> if gridDelta ! (x,y) == 2 then 
+    forM_ (range b) (\(x,y) -> if gridDelta ! (x,y) == Spawn then 
                                     drawCell x y 
-                                else ( if gridDelta ! (x,y) == 1 then 
+                                else ( if gridDelta ! (x,y) == Kill then 
                                     killCell x y 
                                 else return () ) )
     fill
@@ -78,7 +83,7 @@ mainloop gridDelta = do
 delta :: Grid -> Grid -> GridDelta
 delta oldGrid newGrid = listArray c (map f (range c))
             where c = bounds newGrid
-                  f (a,b) = if newGrid ! (a,b) == oldGrid ! (a,b) then 0 else ( if (oldGrid ! (a,b)) then 1 else 2) 
+                  f (a,b) = if newGrid ! (a,b) == oldGrid ! (a,b) then Keep  else ( if (oldGrid ! (a,b)) == Alive then Kill else Spawn) 
 
 -- Stupid stupid hack...
 -- 0 don't do anything
@@ -88,7 +93,9 @@ delta oldGrid newGrid = listArray c (map f (range c))
 swap :: Int -> Int -> Grid -> Grid
 swap x y gr = listArray c (map f (range c))
             where c = bounds gr
-                  f (a,b) = if (x,y) == (a,b) then (not (gr ! (x,y))) else ((gr ! (a,b))) 
+                  f (a,b) = if (x,y) == (a,b) then (n (gr ! (x,y))) else ((gr ! (a,b)))
+                  n Alive = Dead
+                  n Dead = Alive 
 
 
 coordToCell :: Double -> Double -> (Int, Int)
@@ -124,7 +131,7 @@ drawGrid :: Grid -> Render ()
 drawGrid grid = do
     let b = bounds grid
   --  drawCell 1 1
-    mapM_ (\(x,y) -> if grid ! (x,y) == True then drawCell x y else return ()) (range b)
+    mapM_ (\(x,y) -> if grid ! (x,y) == Alive then drawCell x y else return ()) (range b)
     fill
 
 killCell x y = do
@@ -145,7 +152,7 @@ drawField = do
 
 
 
-neighbours :: Grid -> Int -> Int -> [Bool]
+neighbours :: Grid -> Int -> Int -> [Cell]
 neighbours grid x y = map (\x -> grid ! x ) ns
     where ns = [ (x', y') | x' <- [x-1, x, x+1], y' <- [y-1, y, y+1], not(x'==x && y' == y), inbounds (x', y')]
           ((x1,y1),(x2,y2)) = bounds grid
@@ -154,18 +161,18 @@ neighbours grid x y = map (\x -> grid ! x ) ns
 
 neighbourCount :: Grid -> Int -> Int -> Int
 neighbourCount grid x y  = sum (map trans (neighbours grid x y))
-                    where trans False = 0
-                          trans True  = 1
+                    where trans Dead = 0
+                          trans Alive  = 1
 
-action :: Bool -> Int -> Bool
-action True nghc 
-    | nghc <= 1 = False
-    | nghc == 2 = True
-    | nghc == 3 = True
-    | nghc > 3  = False
-action False nghc
-    | nghc == 3 = True 
-    | otherwise = False
+action :: Cell -> Int -> Cell
+action Alive nghc 
+    | nghc <= 1 = Dead
+    | nghc == 2 = Alive
+    | nghc == 3 = Alive
+    | nghc > 3  = Dead
+action Dead nghc
+    | nghc == 3 = Alive 
+    | otherwise = Dead
 
 nextGen :: Grid -> Grid
 nextGen gr = listArray b (map f (range b))
@@ -180,16 +187,16 @@ stringToGrid s = listArray ((1,1), (h, w)) $ linearize s -- (1,1) (1,2) (1,3) ..
 
 linearize s = concatMap f s
         where f         = map trans
-              trans '.' = False
-              trans _   = True
+              trans '.' = Dead
+              trans _   = Alive
 
 gridToString :: Grid -> [String]
 gridToString gr = map l [1..height]
         where ((a,b), (height, width)) = bounds gr
               l i = gridLineString gr i
               gridLineString gr i = concatMap trans [ gr ! (i, x) | x <- [1..width]]
-              trans False = "."
-              trans True  = "#"
+              trans Dead = "."
+              trans Alive  = "#"
 
 
 conc (x:xs) a = r:(conc xs r)
