492c6a20dda02799ba1134ab8b700240ab45b4dc
Martin Ruderer <martin.ruderer@gmail.com>
2013-03-11 21:47:57 +0100

    yay

--- src/gloss2.hs
+++ src/gloss2.hs
@@ -3,9 +3,7 @@ import Graphics.Gloss.Interface.Pure.Animate
 import Data.Array
 import Data.List (unlines, lines, intersperse)
 import Control.Monad (forM_, when, unless, liftM)
-data Cell = Dead | Alive deriving (Eq, Show)
 
-type Grid = Array (Int,Int) Cell
 
 data Animation = A (Float, Float -> Picture)
 
@@ -16,37 +14,48 @@ ft :: Float
 ft = 1
 alist = [A (ft, spawn 10 10),  A (ft, kill 10 10), A (ft, spawn 10 10), A (ft, kill 10 10),  A (ft, spawn 10 10), A (ft, kill 10 10), A (ft, spawn 10 10), A (ft, kill 10 10), A (ft, spawn 10 10), A (ft, kill 10 10), A (ft, spawn 10 10), A (ft, kill 10 10), A (ft, spawn 10 10), A (ft, kill 10 10), A (100000, \t -> blank)]
 main = do
-  --animate ( InWindow "foo" (220,220) (50, 50) ) black (\x -> foo $ (gridlist $ floor (2*x) )) 
-  --animate ( InWindow "foo" (220,220) (50, 50) ) black (\x -> anim alist x)
-  animate ( InWindow "foo" (220,220) (50, 50) ) black (\x -> anim (combConst animationList boarder) x)
-  --animate ( InWindow "foo" (220,220) (50, 50) ) black (\x -> combine [ spawn 10 10, spawn 5 10 ] x)
+  animate ( InWindow "foo" (220,220) (50, 50) ) black 
+      (\x -> anim (combConst animationList border) x)
 
---       return ()
+
 cell x y = color red $ translate ( x) ( y) (circle 4)
-boarder = translate 100 100 $ color white $ lineLoop $ rectanglePath  200 200
-
-combConst :: [Animation] -> Picture -> [Animation]
-combConst l pic = map tr l
-  where tr (A (t, f)) = A (t, combine [f, (\t -> pic)])
-
+border = translate 105 105 $ color white $ lineLoop $ rectanglePath  210 210
 foo grid  = translate (-100) (-100) $ pictures $  
    (map (\((x,y),foo) -> cell (fst $ centerCoords x y) (snd $ centerCoords x y) ) $ 
-    filter (\((x,y), foo) -> foo == Alive) (assocs grid)) ++ [boarder]
+    filter (\((x,y), foo) -> foo == Alive) (assocs grid)) ++ [border]
+rs = 10
+spawn x y time = color (redish (2*time)) $ translate (centerX x y) (centerY x y) (rectangleSolid rs rs) 
+kill x y  time = color (redish (1-2*time)) $ translate (centerX x y) (centerY x y) (rectangleSolid rs rs)
+keepAlive x y  time = color blue $ translate (centerX x y) (centerY x y) (rectangleSolid rs rs)
 
-
-spawn x y time = color (redish (1.5*time)) $ translate (centerX x y) (centerY x y) (thickCircle 2 4 ) 
-kill x y  time = color (redish (1-1.5*time)) $ translate (centerX x y) (centerY x y) (thickCircle 2 4 )
-keepAlive x y  time = color red $ translate (centerX x y) (centerY x y) (thickCircle 2 4)
+spawn' x y time = color (redish (2*time)) $ translate (centerX x y) (centerY x y) (thickCircle 2 4 ) 
+kill' x y  time = color (redish (1-2*time)) $ translate (centerX x y) (centerY x y) (thickCircle 2 4 )
+keepAlive' x y  time = color red $ translate (centerX x y) (centerY x y) (thickCircle 2 4)
 keepDead x y time = blank
 
 redish t = makeColor (t) 0 0 1 
 
 
+combConst :: [Animation] -> Picture -> [Animation]
+combConst l pic = map tr l
+  where tr (A (t, f)) = A (t, combine [f, (\t -> pic)])
+
+funcFromAnimation (A (x, f)) = f
+timeFromAnimation (A (x, f)) = x
+
+combAnim :: [Animation] -> Animation
+combAnim l = A (m, combine l')
+  where m  = maximum $ map timeFromAnimation l
+        l' = map funcFromAnimation l 
+
+anim :: [Animation] -> Float -> Picture
 anim l t  = a (t - start)
   where ((start, end), a) = fromDur t l
 
+fromDur :: Float -> [Animation] -> ((Float, Float), Float -> Picture)
 fromDur t l = head $ filter (\((s,e), f) -> and [(s <= t),(t <= e)] ) (totalFromDur l)
 
+totalFromDur :: [Animation] -> [((Float, Float), Float -> Picture)]
 totalFromDur l = scanl calc ((0,s),abc) (tail l)
     where A (s, abc) = head l
           calc ( (startac, endac), fac) (A (ordur, for)) = ((endac, ordur + endac), for)
@@ -56,38 +65,19 @@ stringToGrid :: [String] -> Grid
 stringToGrid s = listArray ((1,1), (h, w)) $ linearize s -- (1,1) (1,2) (1,3) .. (2,1) ..
         where w = length (head s)
               h = length s
-linearize s = concatMap f s
-        where f         = map trans
+              linearize s = concatMap f s
+              f         = map trans
               trans '.' = Dead
               trans _   = Alive
+
 cellSize = 10
-centerX a b = fst $ centerCoords a b 
-centerY a b = snd $ centerCoords a b 
+
 centerCoords :: Int -> Int -> (Float,Float)
 centerCoords a b = ( fromIntegral nx,  fromIntegral ny )
         where nx = cellSize * a - div cellSize 2
               ny = cellSize * b - div cellSize 2
-
-grid = stringToGrid ["....................", 
-                     ".....#..............", 
-                     "....#...............", 
-                     "....###.............",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "....................",
-                     "...................."]
+centerX a b = fst $ centerCoords a b 
+centerY a b = snd $ centerCoords a b 
 
 
 transf :: GridDelta -> [(Float -> Picture)]
@@ -105,6 +95,9 @@ animationList :: [Animation]
 animationList = map (\g -> A (ft, combine (transf g) ) ) deltaGridList
 
 
+data Cell = Dead | Alive deriving (Eq, Show)
+type Grid = Array (Int,Int) Cell
+
 data Delta = Kill | Spawn | KeepDead | KeepAlive deriving (Eq, Show)
 type GridDelta = Array (Int,Int) Delta
 
@@ -113,11 +106,6 @@ deltaGridList = (deltaGrid emptyGrid (gridList !! 0)) :
 
 emptyGrid = listArray c (map (\x -> Dead) (range c))
       where  c = bounds grid
-
---deltaGridList 0 = deltaGrid emptyGrid grid
---  where emptyGrid = listArray c (map (\x -> Dead) (range c))
---        c = bounds grid
---deltaGridList n = deltaGrid (gridlist (n-1)) (gridlist n)
 
 deltaGrid :: Grid -> Grid -> GridDelta
 deltaGrid oldGrid newGrid = listArray c (map f (range c))
@@ -129,7 +117,6 @@ deltaGrid oldGrid newGrid = listArray c (map f (range c))
                   same Alive Dead  = Kill
 
 gridList = iterate nextGen grid
-
 
 neighbours :: Grid -> Int -> Int -> [Cell]
 neighboursB grid x y = map (\x -> grid ! x ) ns
@@ -183,9 +170,67 @@ deltaToString gr = map l [1..height]
               trans KeepAlive  = "#"
               trans Spawn = "#"
               trans Kill = "."
+grid3 = stringToGrid ["..........#..........", 
+                     "..........#..........", 
+                     "..........#..........", 
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#.........."]
+grid2 = stringToGrid ["..........#..........", 
+                     "..........#..........", 
+                     "..........#..........", 
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "#####################",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#..........",
+                     "..........#.........."]
 
+grid = stringToGrid ["....................", 
+                     ".....#..............", 
+                     "....#...............", 
+                     "....###.............",
+                     "....................",
+                     ".........#..........",
+                     "........#...........",
+                     "........###.........",
+                     "....................",
+                     "....................",
+                     "...............#....",
+                     "..............#.....",
+                     "..............###...",
+                     "....................",
+                     "....................",
+                     "....................",
+                     "....................",
+                     "....................",
+                     "....................",
+                     "...................."]
 
-{-instance Show GridDelta where
-  show foo = unlines (deltaToString foo)
-instance Show Grid where
-  show foo = unlines (gridToString foo)-}
