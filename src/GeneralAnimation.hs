a7b00ffe0032fc83ff0b03d239396f9d23f7e710
Martin <martin.ruderer@gmail.com>
2013-03-26 13:48:28 +0100

    foo

---
+++ src/GeneralAnimation.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DatatypeContexts #-}
+
+import Data.Monoid
+
+
+data (Monoid a) => (Dyna a) = Dyna (Float -> a)
+
+instance (Monoid a) => Monoid (Dyna a) where
+	mempty  = mempty
+	mappend (Dyna f) (Dyna g) = Dyna (\t -> (mappend f g) t)
+
+df1 = Dyna (\t -> [1])
+
+df2 = Dyna (\t -> [2])
+
+s = df1 <> df2
+
+run (Dyna f) t  = f t  
+
+main = do putStrLn $ show $ (run s) 1
